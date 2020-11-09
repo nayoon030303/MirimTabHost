87722579ab4fdb5f6fe66a23e2a45ac7ae16b9c7
kny <kny030303>
2020-11-09 20:26:22 +0900

    Init WebViewActivity

---
+++ app/src/main/java/kr/hs/emirim/s2019s05/mirimtabhost/WebViewActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.emirim.s2019s05.mirimtabhost;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+    }
+}
